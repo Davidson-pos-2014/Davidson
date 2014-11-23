d48ec84eba476e98387db4bf3dd99d7d08c61692
Davidson Gontijo <d.gon@ig.com.br>
2014-11-22 23:49:37 -0200

    Curriculo
    teste

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 //Todo o java script do jogo deve ser colocado aqui
-$(document).ready(function(){
-   document.getElementById("idImg").style.visibility="Hidden"
-   $('#td1').on('click',function(){
-   $(this).html('x') 
-   })   
-})
-
 function Exibefoto(){	
-	document.getElementById("idImg").style.visibility="Visible"
+	document.getElementById("idImg").style.visibility="Visible";
 }
 function Apagafoto(){	
-	document.getElementById("idImg").style.visibility="Hidden"
+	document.getElementById("idImg").style.visibility="Hidden";
 }
+$(document).ready(function(){
+	document.getElementById("idImg").style.visibility="Hidden"; 
+    $.getJSON( "http://rarolabs.com.br:88/alunos.json", function() {
+    }).done(function(data) { 
+        var content = '<option>Pesquisar Currículo de</option>'; 
+        for(var i=0; i <data.length; i++){
+           content += '<option id='+data[i]['id']+'>'+data[i]['nome']+'</option>';
+        }
+        $('#alunos').html(content);
+    });
+
+    $("select").change(function () {
+         var str = "";
+         $( "select option:selected" ).each(function() {
+            str = $( this ).text(); 				
+	    	$.getJSON( "http://rarolabs.com.br:88/alunos.json", function() {
+        	}).done(function(data) {
+        	    for(var i=0; i <data.length; i++){
+                     var nomeAluno = data[i]['nome'] 
+                     var valor  = nomeAluno.length;
+                     var valor1 = str.length;					 
+				     if (str == nomeAluno){
+					     var content = '<p id="idLink"><a id="Valida" href="'+ data[i]['link_html']+'"> Detalhes</a></p>';
+					     $('#idMini').html(data[i]['mini_curriculo']);
+						 $('#idDVLink').html(content);
+                         if (data[i]['link_html'] == ''){ 
+						    alert('Link Detalhes indisponível no momento.');
+						 }						 
+					 }
+        	    }
+        	});
+		});		
+	});
+})
